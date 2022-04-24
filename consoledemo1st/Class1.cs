b10cb3d0fd79ed8913c5452a480771d8cd5ae6f0
yuzhiwei5288 <zw714311681@163.com>
2022-04-24 15:26:48 +0800

    添加项目文件。

---
+++ consoledemo1st/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoledemo1st
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            #region 测试折叠代码
+            Console.WriteLine(Program.GetString(value: 1));
+            Console.ReadKey();
+            #endregion
+        }
+
+
+    }
+}
---
+++ consoledemo1st/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoledemo1st
+{
+    internal static class Program
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        static string GetString(int a)
+        {
+            uint b = 1;
+
+            Console.WriteLine(b);
+            return "a" + a;
+        }
+    }
+}
